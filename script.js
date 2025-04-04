5bc7a7f464412375ee985163d4ab6d0f844b1318
Kristina Faye <hellokristinafaye@gmail.com>
2025-04-04 12:40:26 -0700

    next button functionality

--- script.js
+++ script.js
@@ -95,4 +95,14 @@ function selectAnswer(e) {
     nextButton.style.display = "block";
 }
 
+
+
+nextButton.addEventListener("click", () => {
+    if (currentQuestionIndex < questions.length) {
+        handleNextButton();
+    } else {
+        startQuiz();
+    }
+})
+
 startQuiz();
